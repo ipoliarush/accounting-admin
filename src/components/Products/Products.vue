9df0ebabd6cb9b82c8d6b9f636381e58917fdbe3
Ivan Poliarush <ip.ua97@gmail.com>
2021-08-23 23:03:28 +0300

    add button "add product"

--- src/components/Products/Products.vue
+++ src/components/Products/Products.vue
@@ -4,8 +4,16 @@
       <h4 class="products__title">
         Товары
       </h4>
+      <div class="add">
+        <div class="add__button">
+          <font-awesome-icon class="add__icon" :icon="['fas', 'plus']" />
+          Добавить
+        </div>
+      </div>
     </div>
-    <products-table></products-table>
+    <products-table />
+    
+    
   </div>
 </template>
 
@@ -23,9 +31,25 @@ export default {
 .products {
   .products__head {
     margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
   }
   .products__title {
     @include color('textTitle')
+  }
+
+  .add {
+
+  }
+  .add__button {
+    color: #FFF;
+    background: $bgBtnBlue;
+
+    @include btn;
+    @include btnBlue;
+  }
+  .add__icon {
+    margin-right: 7px;
   }
 }
 </style>
